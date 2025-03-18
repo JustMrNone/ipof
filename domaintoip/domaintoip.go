061b701cd05e07672d7895b42f08f7d4003dc877
Kamyar Bashar <kamyar.bashar@gmail.com>
2025-03-18 22:27:16 +0330

    refactor: simplify DomainToIp function and improve IPv4 address output

--- domaintoip/domaintoip.go
+++ domaintoip/domaintoip.go
@@ -17,40 +17,25 @@ func Ipof() {
 		fmt.Scan(&domain)
 	}
 
-	ipv4, ipv6, err := DomainToIp(domain)
+	ipv4, err := DomainToIp(domain)
+
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	if ipv4 != "" {
-		fmt.Println("IPv4 Address:", ipv4)
-	}
-	if ipv6 != "" {
-		fmt.Println("IPv6 Address:", ipv6)
+	if len(ipv4) > 0 {
+		fmt.Println("IPv4 Addresses:")
+		for _, ip := range ipv4 {
+			fmt.Println(" -", ip)
+		}
 	}
 }
 
-func DomainToIp(domain string) (string, string, error) {
+func DomainToIp(domain string) ([]net.IP, error) {
 	ips, err := net.LookupIP(domain)
-
 	if err != nil {
-		return "", "", fmt.Errorf("could not get the IPs: %v", err)
+		return nil, fmt.Errorf("could not get the IPs: %v", err)
 	}
-
-	var ipv4, ipv6 string
-
-	for _, ip := range ips {
-		if ipv4 == "" && ip.To4() != nil {
-			ipv4 = ip.String()
-		} else if ipv6 == "" && ip.To4() == nil {
-			ipv6 = ip.String()
-		}
-	}
-
-	if ipv4 == "" && ipv6 == "" {
-		return "", "", fmt.Errorf("no IPs found for domain: %s", domain)
-	}
-
-	return ipv4, ipv6, nil
+	return ips, nil
 }
